b438d115d3e7afaced0f1bc543408c4763f4574b
Yutaro HIGASHI <yutaro.higashi@notteco.jp>
2018-12-15 13:30:07 +0900

    Added Text to speech

--- app.rb
+++ app.rb
@@ -3,6 +3,14 @@ require 'bundler'
 Bundler.setup(:default, :ci)
 
 require 'sinatra'
+require "google/cloud/text_to_speech/v1"
+
+helpers do
+  def h(text)
+    Rack::Utils.escape_html(text)
+  end
+end
+
 get '/' do
   'Put this in your pipe & smoke it!'
 end
@@ -20,3 +28,15 @@ end
 get '/camera' do
   `fswebcam public/output.jpg`
 end
+
+get '/speech/:text' do
+  text_to_speech_client = Google::Cloud::TextToSpeech::V1.new
+  input = {text: h(params[:text])}
+  voice = {language_code: "ja-JP"}
+  audio_config = {audio_encoding: Google::Cloud::Texttospeech::V1::AudioEncoding::MP3}
+  response = text_to_speech_client.synthesize_speech input, voice, audio_config
+  File.open File.dirname(__FILE__) + "/assets/audio/output.mp3", "w" do |file|
+    file.write response.audio_content
+  end
+end
+
